580be6086ba08373dbde304fdda9f684d04cd8db
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-07-01 23:45:42 +0300

    phpDisplayVariables

---
+++ phpDisplayVariables.php
@@ -0,0 +1,10 @@
+<?php
+	$txt1 = "Learn PHP";
+	$txt2 = "SoftUni.bg";
+	$x = 5;
+	$a = 4;
+	$res = $x + $a;
+	echo "<h2>$txt1</h2>";
+	echo "Study PHP at $txt2<br>";
+	echo "Result = $a + $x => $res";
+?>
