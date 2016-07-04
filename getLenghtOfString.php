395ab861322f4dc3d5d31de2637b4dd404ddb748
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-07-04 13:19:15 +0300

    count lenghtof string

---
+++ getLenghtOfString.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lenght of string</title>
+</head>
+<body>
+<h2>
+<?php
+	echo strlen("Hello world");
+?>
+</h2>
+</body>
+</html>
