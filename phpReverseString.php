ff7cf58f7103da1a6159ec22180f724cb11476fc
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-07-04 14:07:09 +0300

    phpReverseString

---
+++ phpReverseString.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h2>
+		<?php
+		$input = "Hello world";
+		echo "$input <br>";
+		echo strrev($input);
+		?>
+	</h2>
+</body>
+</html>
