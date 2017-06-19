7c424953c51993bc52afe877b7298a871ef2f67c
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-06-19 16:36:00 +0300

    add real escape strings

--- task/insertData.php
+++ task/insertData.php
@@ -5,9 +5,9 @@
     if(!$connection){
         echo "Not connected";
     }
-    $firstName = $_POST['inputFirstName'];
-    $lastName = $_POST['inputLastName'];
-    $favColor = $_POST['inputFavoriteColor'];
+    $firstName = mysqli_real_escape_string($connection,$_POST['inputFirstName']);
+    $lastName = mysqli_real_escape_string($connection,$_POST['inputLastName']);
+    $favColor = mysqli_real_escape_string($connection,$_POST['inputFavoriteColor']);
     $sql = "INSERT INTO user_info(first_name,last_name,favorite_color) VALUES ('$firstName','$lastName','$favColor')";
     $startQuery = mysqli_query($connection,$sql);
     if(!$startQuery){
